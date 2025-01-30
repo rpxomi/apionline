57ac0638e049fa6cf9960db35024567ce9f059dd
rpxomi <31228150+rpxomi@users.noreply.github.com>
2025-01-29 21:15:09 -0300

    Update views.py
    Se agrega la vista de la Tarjeta para poder elegir desde la URL filtar informacion segun lo que se escriba.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Producto
-from .serializers import ProductoSerializer
+from .models import Producto, Tarjeta
+from .serializers import ProductoSerializer, TarjetaSerializer
 
-# Create your views here.
+
 class ProductoViewSet(ModelViewSet):
     serializer_class = ProductoSerializer
 
@@ -11,4 +11,15 @@ class ProductoViewSet(ModelViewSet):
         categoria = self.kwargs.get('categoria', None)
         if categoria is not None:
             queryset = queryset.filter(categoria=categoria)
+        return queryset
+
+
+class TarjetaViewSet(ModelViewSet):
+    serializer_class = TarjetaSerializer
+
+    def get_queryset(self):
+        queryset = Tarjeta.objects.all()
+        encabezado = self.kwargs.get('encabezado', None)
+        if encabezado is not None:
+            queryset = queryset.filter(encabezado=encabezado)
         return queryset
