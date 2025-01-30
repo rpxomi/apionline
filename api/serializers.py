c9a1031f07b7273cb9d892ed1d21cfdeb28427f0
rpxomi <31228150+rpxomi@users.noreply.github.com>
2025-01-29 21:13:26 -0300

    Update serializers.py
    Se agrega el serializador para elegir que informacion mostrar de la tabla Tarjeta

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Producto
+from .models import Producto, Tarjeta
+
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = ['id', 'titulo', 'descripcion', 'precio', 'imagen_url', 'categoria']
+
+
+class TarjetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarjeta
+        fields = ['id', 'encabezado', 'cuerpo']
