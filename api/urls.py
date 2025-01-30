dcfe7f617a98bb10ad2edb55ac4357e9ab1d0712
rpxomi <31228150+rpxomi@users.noreply.github.com>
2025-01-29 21:16:38 -0300

    Update urls.py
    Se agrega mostar todas las tarjetas y mostrar tarjetas filtrandolas segun el encabezado (hombre, mujer, niño)

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ProductoViewSet
+from .views import ProductoViewSet, TarjetaViewSet
+
 
 urlpatterns = [
     path('productos/', ProductoViewSet.as_view({'get':'list'}), name='productos'),
     path('productos/<int:pk>/', ProductoViewSet.as_view({'get':'retrieve'}), name='producto'),
-    path('productos/<str:categoria>/', ProductoViewSet.as_view({'get':'list'}), name='productos_categoria')
+    path('productos/<str:categoria>/', ProductoViewSet.as_view({'get':'list'}), name='productos_categoria'),
+
+    path('tarjetas/', TarjetaViewSet.as_view({'get':'list'}), name='tarjetas'),
+
+    # retrieve es solo con pk (primary key) clave primaria, o sea el id numerico
+    path('tarjetas/<str:encabezado>/', TarjetaViewSet.as_view({'get':'list'}), name='tarjeta'),
 ]
