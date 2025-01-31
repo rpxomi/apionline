4aa32a30e74e3d4ef271fea422266a630a4b0f32
rpxomi <31228150+rpxomi@users.noreply.github.com>
2025-01-31 03:39:38 -0300

    Update views.py
    Se agrega busqueda por terminos en la URL

--- api/views.py
+++ api/views.py
@@ -2,24 +2,38 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Producto, Tarjeta
 from .serializers import ProductoSerializer, TarjetaSerializer
 
+# - - -
 
 class ProductoViewSet(ModelViewSet):
     serializer_class = ProductoSerializer
 
+
     def get_queryset(self):
         queryset = Producto.objects.all()
+
         categoria = self.kwargs.get('categoria', None)
         if categoria is not None:
             queryset = queryset.filter(categoria=categoria)
+
+        # busqueda a traves de la URL
+        # http://127.0.0.1:8000/api/productos/hombre/?titulo=[ingresar palabra]
+        titulo = self.request.query_params.get('titulo', None)
+        if titulo:
+            queryset = queryset.filter(titulo__icontains=titulo)
+
         return queryset
 
+# - - -
 
 class TarjetaViewSet(ModelViewSet):
     serializer_class = TarjetaSerializer
 
+
     def get_queryset(self):
         queryset = Tarjeta.objects.all()
+
         encabezado = self.kwargs.get('encabezado', None)
         if encabezado is not None:
             queryset = queryset.filter(encabezado=encabezado)
+        
         return queryset
